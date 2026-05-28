ddc72a8ba13064948c411f94d668ece9e7cb9e27
6510615245 <6510615245@student.tu.ac.th>
2026-05-29 01:36:47 +0700

    Initial Commit

---
+++ ReflexTest/ReflexTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReflexTestApp.swift
+//  ReflexTest
+//
+//  Created by Ploypan on 29/5/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct ReflexTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReflexTestTests/ReflexTestTests.swift
@@ -0,0 +1,18 @@
+//
+//  ReflexTestTests.swift
+//  ReflexTestTests
+//
+//  Created by Ploypan on 29/5/2569 BE.
+//
+
+import Testing
+
+struct ReflexTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
